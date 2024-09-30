6374313272eeb07b59ef880024252550c83b030b
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2024-09-30 12:12:07 +0530

    backtracking problems added

---
+++ backtracking/gridwaysMethod.java
@@ -0,0 +1,22 @@
+public class gridwaysMethod {
+
+    public static int gridWays(int i, int j, int n, int m) {
+        // base case
+        if (i == n - 1 & j == m - 1) {
+            return 1;
+        } else if (i == n || j == m) {
+            return 0;
+        }
+
+        // reccursion
+        int w1 = gridWays(i + 1, j, n, m);
+        int w2 = gridWays(i, j + 1, n, m);
+        return w1 + w2;
+    }
+
+    public static void main(String args[]) {
+        int n = 3; // row
+        int m = 3; // coloumn
+        System.out.println(gridWays(0, 0, n, m));
+    }
+}
