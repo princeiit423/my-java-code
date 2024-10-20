12f40bfcf839dc812ad4d6853739135f107c0fbf
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2024-10-20 12:26:16 +0530

    more problems added

--- Greedy_Algorithm/Activity_Selection_endtime_not_sorted.java
+++ Greedy_Algorithm/Activity_Selection_endtime_not_sorted.java
@@ -1,30 +1,39 @@
 package Greedy_Algorithm;
 
+import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Comparator;
 
-public class Acitivty_selection {
+public class Activity_Selection_endtime_not_sorted {
     public static void main(String[] args) {
-        // ye question me ending time sorted hai islye thoda easy hai end time sort nhi
-        // krna pad rha hai,
-        // lekin vhut question me end time sorted nhi rhega to wo problem solve krne ana
-        // chaye, ye problem dusra programe me kiye hai check it.
 
         int start[] = { 1, 3, 0, 5, 8, 5 };
         int end[] = { 2, 4, 6, 7, 9, 9 };
+
+        // logic to sort end time using 2D array
+
+        int activities[][] = new int[start.length][3];
+        for (int i = 0; i < start.length; i++) {
+            activities[i][0] = i;
+            activities[i][1] = start[i];
+            activities[i][2] = end[i];
+        }
+        // sorting
+        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
 
         int maxAct = 0;
         ArrayList<Integer> ans = new ArrayList<>();
         // 1st activity
         maxAct = 1;
-        ans.add(0);
-        int lastEnd = end[0];
+        ans.add(activities[0][0]);
+        int lastEnd = activities[0][2];
         // for loop to check select activity is greater than last activity chosesen end
         // time
         for (int i = 1; i < start.length; i++) {
-            if (start[i] >= lastEnd) {
+            if (activities[i][1] >= lastEnd) {
                 maxAct++;
-                ans.add(i);
-                lastEnd = end[i];
+                ans.add(activities[i][0]);
+                lastEnd = activities[i][2];
             }
         }
         System.out.println("Max Activities: " + maxAct);
