53d01cda6036c509afa323bb4c9d0d75ecd1a4fa
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-02-03 13:28:03 +0530

    problem added

---
+++ Stacks/reverseString.java
@@ -0,0 +1,24 @@
+//package Stacks;
+
+import java.util.*;
+
+public class reverseString {
+    public static void reverseString(String s, Stack<Character> stack) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            stack.push(ch);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char sb = stack.pop();
+            res.append(sb);
+        }
+        System.out.print(res);
+    }
+
+    public static void main(String args[]) {
+        String s = "Hello";
+        Stack<Character> stack = new Stack<>();
+        reverseString(s, stack);
+    }
+}
