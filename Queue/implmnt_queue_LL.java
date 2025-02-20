d15f52e7e5b5a2ab32ffd8f3238ef3ffde9db967
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-02-21 00:12:01 +0530

    more prob added

---
+++ Queue/implmnt_queue_LL.java
@@ -0,0 +1,73 @@
+//package Queue;
+
+public class implmnt_queue_LL {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class Queue {
+        static Node head = null;
+        static Node tail = null;
+
+        // is empty queue
+        public static boolean isEmpty() {
+            return head == null && tail == null;
+        }
+
+        // add in queue
+
+        public static void add(int data) {
+            Node newNode = new Node(data);
+            if (head == null && tail == null) {
+                head = tail = newNode;
+                return;
+            }
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        // remove in queue
+        public static int remove() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            int front = head.data;
+            if (tail == head) {
+                head = tail = null;
+            } else {
+                head = head.next;
+            }
+            return front;
+        }
+
+        // peek in queue
+
+        public static int peek() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return head.data;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Queue q = new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
