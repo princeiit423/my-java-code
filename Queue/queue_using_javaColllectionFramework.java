fcd3e752760e18773699d0eda72564900e6cd425
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-02-21 00:20:21 +0530

    queue using JCF

---
+++ Queue/queue_using_javaColllectionFramework.java
@@ -0,0 +1,19 @@
+//package Queue;
+
+import java.util.*;
+
+public class queue_using_javaColllectionFramework {
+    public static void main(String[] args) {
+
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
