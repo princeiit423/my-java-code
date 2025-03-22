8ff338a6e7ac41fd8cc4825f5a77b4cc237b750a
LAPTOP-V11F3UN6 <princeiit423@gmail.com>
2025-03-22 11:37:17 +0530

    modify

--- Dynamic Programming/targerSum_tabulation.java
+++ Dynamic Programming/targerSum_tabulation.java
@@ -18,12 +18,24 @@ public class targerSum_tabulation {
                 }
             }
         }
+        print(dp);
         return dp[n][sum];
+    }
+
+    public static void print(boolean dp[][]) {
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
         int arr[] = { 4, 2, 7, 1, 3 };
         int sum = 10;
+
         System.out.println(targetSum(arr, sum));
     }
 }
